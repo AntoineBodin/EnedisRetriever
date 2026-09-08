d448eb2e55e955b70ae7d35e4be7d03e75cacbf9
Antoine Bodin <antoine-bodin@hotmail.fr>
2026-09-08 10:43:25 +0200

    add automatic ef migrations

--- EnedisRetriever/EnedisRetriever/Program.cs
+++ EnedisRetriever/EnedisRetriever/Program.cs
@@ -69,6 +69,13 @@ namespace EnedisRetriever
 
             var app = builder.Build();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider
+                    .GetRequiredService<AppDbContext>();
+
+                dbContext.Database.Migrate();
+            }
             //app.UseHttpsRedirection();
 
             app.UseCors("Frontend");
